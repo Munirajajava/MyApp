3f86be31836ea7c84d86a2cf09dfb5ddb27dbc47
Muniraja <munirajaalakunta2000@gmail.com>
2025-12-05 19:32:09 +0530

    removing unused variables

--- Employee.java
+++ Employee.java
@@ -1,12 +1,5 @@
 class Employee {
-  int eid;
-  String ename;
-  double salary;
-  String address;
-  int deptno;
-
-  double d = 0.0;
-
+  
   boolean flag = false;
 
 public static void main(String[] args) {
